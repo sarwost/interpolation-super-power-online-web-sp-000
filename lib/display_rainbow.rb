a21d7678ef3af5c2f9cc1b2bb454e941cacaabb6
Sarah Wostenberg <swostenberg@gmail.com>
2020-12-04 16:16:01 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
 # Write your #display_rainbow method here
 def display_rainbow(friYAY)
-   print "R: #{friYAY[0]}, O: #{friYAY[1]}, Y: #{friYAY[2]}, G: #{firYay[3]}, B: #{friYAY[4]}, B: #{friYAY[5]}, I: #{friYAY[6]}"
+   puts "R: #{friYAY[0]}, O: #{friYAY[1]}, Y: #{friYAY[2]}, G: #{firYay[3]}, B: #{friYAY[4]}, B: #{friYAY[5]}, I: #{friYAY[6]}"
 end 
