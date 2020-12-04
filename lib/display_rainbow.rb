dca6caa4d7fc786ff3fd2e5cc477d5b56ed9a5b4
Sarah Wostenberg <swostenberg@gmail.com>
2020-12-04 16:12:44 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,10 +2,10 @@
 require "pry"
 def display_rainbow(friYAY)
   
-   puts "R: #{friYAY[0]}," 
-  # puts "O: #{display_rainbow[1]},"
-  # puts "Y: #{display_rainbow[2]},"
-  # puts "G: #{display_rainbow[3]},"
+   puts "R: #{friYAY[0]}, O: #{friYAY[1]}, Y: #{friYAY[2]}, G: #{firYay[3]}, B: #{friYAY[4]}, 
+   
+   "
+   #{display_rainbow[3]},"
   # puts "B: #{display_rainbow[4]},"
   # puts "I: #{display_rainbow[5]},"
   # puts "V: #{display_rainbow[6]},"
